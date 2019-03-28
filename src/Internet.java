5a972eb567a07c4fd3e72591b559bd0d8aeed286
Adam966 <adam.ivan@akademiasovy.sk>
2019-03-28 13:03:55 +0100

    add files

--- src/Internet.java
+++ src/Internet.java
@@ -1,7 +1,5 @@
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
-
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,7 +7,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class Internet {
-    public void sendGET() throws IOException, ParseException {
+    public void sendGET() throws IOException {
         URL obj = new URL("https://api.exchangeratesapi.io/latest?symbols=USD");
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         con.setRequestMethod("GET");
@@ -25,22 +23,15 @@ public class Internet {
             while ((inputLine = in.readLine()) != null) {
                 response.append(inputLine);
             }
+
+            JsonObject jobj = new Gson().fromJson(String.valueOf(response), JsonObject.class);
+            JsonObject currency = (JsonObject) jobj.get("rates");
+            float curr = currency.get("USD").getAsFloat();
+
+
             in.close();
-
-            try {
-                JSONParser parser = new JSONParser();
-
-                JSONObject json = (JSONObject) parser.parse(inputLine);
-                System.out.println(json);
-
-                System.out.println(response);
-            } catch (ParseException pe) {
-
-            }
-
         } else {
             System.out.println("GET request not worked");
         }
-
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,14 @@
 import Exeptions.BillExeptions;
-import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws BillExeptions {
+    public static void main(String[] args) throws BillExeptions, IOException {
         Application app = Application.getInstance();
 
         app.example();
 
         Internet request = new Internet();
-        try {
-            request.sendGET();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+        request.sendGET();
     }
 }
